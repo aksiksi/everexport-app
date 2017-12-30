ab0de6701d58372a515101a18331135abb0f9e07
Assil Ksiksi <cyph0nik@gmail.com>
2017-12-30 01:32:00 -0500

    Created everexport NPM package, use it Electron app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const everexport = require('./everexport/everexportjs-opt')
-const token = "S=s1:U=93aa5:E=167af159bc9:C=16057646f00:P=1cd:A=en-devtoken:V=2:H=62d4673ee2568acef054ff56e5c1e41d"
+const everexport = require('everexport')
+const token = 'S=s1:U=93aa5:E=167af159bc9:C=16057646f00:P=1cd:A=en-devtoken:V=2:H=62d4673ee2568acef054ff56e5c1e41d'
 const sandbox = true
 const ex = new everexport.EvernoteExporter(token, sandbox)
 
